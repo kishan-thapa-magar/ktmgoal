3f3addbf954149fdb388e49b019004a4d6c3457b
kishan-thapa-magar <kishanthapamagar9866494950@gmail.com>
2022-03-27 21:16:32 +0545

    goal-complete

---
+++ include/nav.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+       
+     
+            <ul class="nav-bar-ul">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="goal.php">Goals</a>
+                </li>
+
+
+            </ul>
+            
+          
+            <form class="d-flex" action="db/logout.php">
+
+                <button class="btn btn-outline-danger" type="submit">logout</button>
+            </form>
+        </div>
+    </div>
+</nav>
